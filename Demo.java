b8e6e788e3ef164d4c546e0026859d991c70ff8a
Vipul Tembulwar <vipullabkh@gmail.com>
2022-05-27 17:11:25 +0530

    Demo.java created

---
+++ Demo.java
@@ -0,0 +1,16 @@
+class Date{
+    private int day;
+    private int month;
+    private int year;
+
+    public Date(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public String toString() {
+        return (day +" "+month+" "+year);
+    }
+}
+
